1446c892d0a5fc7d868cda40378670287b9d58a2
Isa Goksu <isa.goksu@gmail.com>
2013-11-18 15:08:03 +0200

    08 - Functions

---
+++ src/chapter08/Functions.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * This code is developed and owned by Borsa Istanbul A.S.
+ * The distribution of this code by any means is prohibited.
+ * You cannot copy/share/distribute/deliver/use/modify/move without having
+ * a permission to do so. Use it with your own risk.
+ *
+ * All rights reserved. Copyright (c) 2013.
+ ******************************************************************************/
+
+package chapter08;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ * @version 1.0
+ */
+public class Functions {
+   public static void main(String[] args) {
+      List<Integer> numbers = Arrays.asList(1, 2, 3);
+
+      // accepting integer, returning double
+      System.out.println(mapIt(numbers, x -> Math.pow(10, x)));
+   }
+
+   public static <T, R> List<R> mapIt(List<T> candidates, Function<T, R> mapper) {
+      List<R> mappedList = new ArrayList<>();
+
+      for(T candidate : candidates) {
+         mappedList.add(mapper.apply(candidate));
+      }
+
+      return mappedList;
+   }
+}
